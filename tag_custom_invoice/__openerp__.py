fb900c2d825a1dd0482194cc2dbb69b66f5bc249
TAGSBM <agardiner@tagsbm.com>
2015-12-02 11:13:50 +1100

    add abn and acn
    add abn and acn

--- tag_custom_invoice/__openerp__.py
+++ tag_custom_invoice/__openerp__.py
@@ -1,11 +1,12 @@
 {
     'name': "Custom Invoices(TAGSBM)",
-    'version': "1.0",
+    'version': "1.1",
     'author': "TAGSBM",
     'category': "Tools",
     'summary': "Modifies invoice to display",
     'data': [
         'ir.ui.view.csv',
+        'res_company.xml',
     ],
     'demo': [],
     'depends': ['web', 'account','report'],
